7f9ec29e788fb706fa36be66c86ee4b04a5efc5c
agun36 <aguntoroti10@gmail.com>
2024-01-02 17:45:18 +0100

    main

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -15,10 +15,4 @@ function updateCounter(){
 	$("#count").text('${count} clicks on the button');
 }
 
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button id="start">Click here to get started</button>');
-$('body').append('<p id="count"></p>');
-$('body').append('<p>Copyright - Holberton School</p>');
-
 $('#start').on('click', debounce(updateCounter, 500));
