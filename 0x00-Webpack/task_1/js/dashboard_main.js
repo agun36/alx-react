08d128c83e7a75478e9f4bdf2538a6aaa0918ee1
agun36 <aguntoroti10@gmail.com>
2024-01-02 17:19:04 +0100

    dashboard_main.js

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+
 import _ from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
@@ -7,11 +8,12 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let count = 0;
 
-function updateCounter() {
-  count++;
-  $("#count").html(`${count} clicks on the button`);
-};
+let counter = 0;
+
+function updateCounter(){
+	counter ++;
+	$("#count").html('${counter} clicks on the button');
+}
 
 $('button').on('click', _.debounce(updateCounter, 500));
