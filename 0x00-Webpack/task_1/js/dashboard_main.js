c7678b66a97cec1dbded96a827d9d8d84e68fa9a
agun36 <aguntoroti10@gmail.com>
2024-01-02 17:38:45 +0100

    build

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -8,7 +8,6 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-
 let count = 0;
 
 function updateCounter(){
