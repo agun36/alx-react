c6889761dc59a5a7ba3c8a8e7b3be9050772b6b3
agun36 <aguntoroti10@gmail.com>
2024-01-03 17:33:49 +0100

    main

--- 0x01-react_intro/task_3/dashboard/src/App.js
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './holberton-logo.jpeg';
 import './App.css';
+import { getFooterCopy, getFullYear } from './utils';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" width="200px" height="200px" />
+        <h1>School dashboard</h1>
       </header>
+      <div className="App-body">
+        <p>
+          Login to access the full dashboard
+        </p>
+        <form>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" />
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
+          <button type="submit">OK</button>
+        </form>
+      </div>
+      <footer className="App-footer">
+        <p>
+        Copyright {getFullYear()} - {getFooterCopy()}
+        </p>
+      </footer>
     </div>
   );
 }
--- 0x01-react_intro/task_3/dashboard/src/App.test.js
+++ 0x01-react_intro/task_3/dashboard/src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('App renders without crashing', () => {
+  shallow(<App />);
+});
+
+test('App renders a div with the class App-header', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('.App-header').length).toBe(1);
+});
+
+test('App renders a div with the class App-body', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('.App-body').length).toBe(1);
+});
+
+test('App renders a div with the class App-footer', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('.App-footer').length).toBe(1);
 });
