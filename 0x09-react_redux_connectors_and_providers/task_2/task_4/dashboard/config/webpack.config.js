e322677045af1b767206a0d0db6929c64c5b88b4
agun36 <aguntoroti10@gmail.com>
2024-02-12 08:58:11 +0100

    main

---
+++ 0x09-react_redux_connectors_and_providers/task_2/task_4/dashboard/config/webpack.config.js
@@ -0,0 +1,56 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path")
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, "dist"),
+        },
+        hot: true,
+        compress: true,
+        port: 8564,
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                use:{
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                },
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/
+            },
+            {
+                use: ["style-loader", "css-loader"],
+                test: /\.css$/i
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    "file-loader",
+                    {
+                        loader: "image-webpack-loader",
+                        options: {
+                            bypassOnDebug: true, // webpack@1.x
+                            disable: true, // webpack@2.x and newer
+                        },
+                    },
+                ],
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".json"]
+    },
+    devtool: "inline-source-map",
+}
